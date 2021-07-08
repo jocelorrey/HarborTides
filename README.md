1f5cc5ff9bbeb03aa600f8fdcbf91bf5f364688f
jocelorrey <jocelorrey@gmail.com>
2021-07-08 06:31:47 -0400

    More robust to input errors

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Simple little project to practice web scraping. Gets tidal data for a specified 
 ### To Run:
 
 1. Run `python3 ./get_tides.py`
-2. When prompted, enter a harbor and the two-letter abbreviation for the state in which it is located
-   (ex: "Portsmouth Harbor" and "NH")
-3. The program navigates to the tides tab for your harbor of interest on www.usharbors.com and grabs the tide table html. Currently it just prints the html as a placeholder. Eventually it'll do something cooler with it (ex. maybe make a simple API where you can run different commands to get high tide for today, low tide for the 17th of the month, etc.)
+2. When prompted, enter a harbor and the two-letter abbreviation or full name of the state in which it is located. If the state you entered is invalid or does not border the ocean, you will be prompted to enter a new state.
+3. Based on your input harbor and state, the program generates a search url and attempts to navigate to the tides tab for your harbor of interest on www.usharbors.com. If your search url is invalid, you will be asked if you want to try again with a new state/harbor or quit the program. If your search url is valid, the program will grab the tide table html.
+
+Next steps: Eventually it'll do something cool/helpful with the html (ex. let you run different commands to get high tide for today, low tide for the 17th of the month, etc.)
--- get_tides.py
+++ get_tides.py
@@ -2,27 +2,77 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
-def scrape_data(harbor, state):
-  harbor_location = (harbor.replace(' ', '-') + '-' + state).lower() # ex. "York Harbor" + "ME" -> "york-harbor-me"
+def get_state_abbrev(state_name):
+  ocean_states = {'alabama': 'AL', 'alaska': 'AK', 'california': 'CA',
+  'connecticut': 'CT', 'delaware': 'DE', 'florida': 'FL', 'georgia': 'GA',
+  'hawaii': 'HI', 'louisiana': 'LA', 'maine': 'ME', 'maryland': 'MD',
+  'massachusetts': 'MA', 'mississippi': 'MS', 'new hampshire': 'NH',
+  'new jersey': 'NJ', 'new york': 'NY', 'north carolina': 'NC',
+  'oregon': 'OR', 'rhode island': 'RI', 'south carolina': 'SC',
+  'texas': 'TX','vermont': 'VT', 'virginia': 'VA', 'washington': 'WA'}
 
+  state = state_name.lower()
+  if state in ocean_states.keys():
+    return ocean_states[state]
+  else:
+    return ''
+
+def get_search_url():
+  # Get user input
+  state = input("US State: ").lower()
+  harbor = input("Harbor: ").lower()
+
+  # Clean up user input
+  if 'harbor' not in harbor:
+    harbor += " harbor"
+
+  while len(state) != 2:
+    state = get_state_abbrev(state)
+    if state  == '':
+      state = input("Please enter a valid US state bordering the ocean: ").lower()
+
+  # Generate url
+  harbor_location = (harbor.replace(' ', '-') + '-' + state)
+  return 'https://www.usharbors.com/harbor/' + state + '/' + harbor_location + '/tides#monthly-tide-chart'
+
+def get_tide_table(search_url):
   driver = webdriver.Chrome()
-  driver.get('https://www.usharbors.com/harbor/' + state + '/' + harbor_location + '/tides#monthly-tide-chart')
+  driver.get(search_url)
   content = driver.page_source
   soup = BeautifulSoup(content, 'html.parser')
 
-  tide_table = soup.find("table", {"class": "tides"})
-  print("tide_table:", tide_table)
+  if soup.h1.string == 'Oops! That page can’t be found.':
+    driver.close()
+    return None
+
+  else:
+    driver.close()
+    return soup.find("table", {"class": "tides"})
   
 
 # Execution
-harbor = input("Harbor: ") # ex. "York Harbor"
-state = input("State: ") # ex. "ME"
-scrape_data(harbor, state)
+# For testing:
+# bad_url = 'https://www.usharbors.com/harbor/pa/newark-harbor-pa/tides#monthly-tide-chart'
+# good_url = 'https://www.usharbors.com/harbor/maine/york-harbor-me/tides/'
+
+url = get_search_url()
+tide_table = get_tide_table(url)
+
+# Deal with errors
+while tide_table is None:
+  print("The harbor you searched for cannot be found.")
+  try_again = input('Try again? [y/n] ').lower()
+  if try_again in ['y', 'yes']:
+    url = get_search_url()
+    tide_table = get_tide_table(url)
+  else:
+    quit()
+
+print("Woo - got a valid url!")
 
 """
 Useful Resources:
 1. Tutorial: https://www.edureka.co/blog/web-scraping-with-python/
-2. Search Bar Query: https://datascience.stackexchange.com/questions/11730/how-to-scrape-a-website-with-a-searchbar
-3. HTTPResponse Objects: https://docs.python.org/3/library/http.client.html
-4. Beautiful Soup - Find Table: https://stackoverflow.com/questions/33766740/beautifulsoup-find-table-with-specified-class-on-wikipedia-page
+2. Beautiful Soup Docs: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+3. Beautiful Soup - Find Table: https://stackoverflow.com/questions/33766740/beautifulsoup-find-table-with-specified-class-on-wikipedia-page
 """
